bd7bdc2ac3ab4e6026960c3c0b84a41b9b5274ab
Luca Rillosi <luca.rillosi+git@gmail.com>
2022-04-30 18:51:33 +0200

    add pytorch dataloader

--- src/ptl.py
+++ src/ptl.py
@@ -15,21 +15,37 @@ import pytorch_lightning as pl
 class LitAutoEncoder(pl.LightningModule):
     def __init__(self):
         super().__init__()
-        self.encoder = nn.Sequential(nn.Linear(128, 128), nn.ReLU(), nn.Linear(128, 3))
-        # self.decoder = nn.Sequential(nn.Linear(3, 128), nn.ReLU(), nn.Linear(128, 28 * 28))
+        self.encoder = nn.Sequential(
+            nn.Conv1d(1, 32, kernel_size=2),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+            nn.Conv1d(1, 32, kernel_size=2),
+            nn.ReLU(),
+            nn.MaxPool1d(kernel_size=2),
+
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(128, 32),
+            nn.ReLU(),
+            nn.Linear(32, 4),
+            nn.Softmax(),
+        )
 
     def forward(self, x):
-        # in lightning, forward defines the prediction/inference actions
-        embedding = self.encoder(x)
-        return embedding
+        """in lightning, forward defines the prediction/inference actions"""
+        x = self.encoder(x)
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = self.classifier(x)
+        return x
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop. It is independent of forward
         x, y = batch
         x = x.view(x.size(0), -1)
-        z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = F.mse_loss(x_hat, x)
+        x = self.encoder(x)
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        y_hat = self.classifier(x)
+        loss = F.cross_entropy(y_hat, y)
         self.log("train_loss", loss)
         return loss
 
@@ -41,23 +57,32 @@ class LitAutoEncoder(pl.LightningModule):
 # %%
 from eval import CWRUA, CWRUB, read_dataset
 from model import INPUT_LENGTH
-X_train, y_train = read_dataset(CWRUA, input_length=INPUT_LENGTH)
-X_test, y_test = read_dataset(CWRUB, input_length=INPUT_LENGTH)
+from torch.utils.data import TensorDataset, DataLoader
+
+
+def torch_prepare_dataloader(x, y):
+    x = torch.Tensor(x)
+    y = torch.Tensor(y)
+    x = torch.rand((512, 1, 128))
+    y = torch.rand((512, 1, 4))
+    dataset = TensorDataset(x, y)
+    return DataLoader(dataset, batch_size=32)
+
+
+x, y = read_dataset(CWRUA, input_length=INPUT_LENGTH)
+train_loader = torch_prepare_dataloader(x, y)
+x, y = read_dataset(CWRUB, input_length=INPUT_LENGTH)
+test_loader = torch_prepare_dataloader(x, y)
+
+x, y = next(iter(train_loader))
+print(x.shape)
+print(y.shape)
 
 # %%
-dl = DataLoader([X_train, y_train])
-
-for (x, y) in dl:
-    print(x.shape)
-    print(y.shape)
-# %%
-
-dataset = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor())
-train, val = random_split(dataset, [55000, 5000])
 
 autoencoder = LitAutoEncoder()
 trainer = pl.Trainer()
-trainer.fit(autoencoder, DataLoader(train), DataLoader(val))
+trainer.fit(autoencoder, train_loader, test_loader)
 
 # %%
 
