dc752489259d3f6f825749caeb9a6a0aecc92023
Luca Rillosi <luca.rillosi+git@gmail.com>
2022-05-16 19:07:00 +0200

    improve trainer

--- trainer_main.py
+++ trainer_main.py
@@ -5,9 +5,6 @@ from pytorch_lightning.callbacks import ModelSummary, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import Callback, ModelCheckpoint
 import pytorch_lightning as pl
-import tensorflow as tf
-import tensorboard as tb
-tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
 
 # PARSE ARGS
 parser = ArgumentParser()
@@ -20,8 +17,9 @@ parser = MyModel.add_argparse_args(parser)
 # add all the available trainer options to argparse
 # ie: now --accelerator --devices --num_nodes ... --fast_dev_run all work in the cli
 parser = pl.Trainer.add_argparse_args(parser)
+logparser = parser.add_argument_group("MyLogger")
+logparser.add_argument("--experiment_name", "-n", type=str, default="default")
 args = parser.parse_args()
-# args, unknown = parser.parse_known_args()
 
 # LOAD DATA
 data_module = MyDataModule.from_argparse_args(args)
@@ -38,10 +36,10 @@ class CheckpointTracker(Callback):
 
 # PREPARE TRAINER
 tracker = CheckpointTracker()
-logger = TensorBoardLogger(save_dir=".", log_graph=True)
+logger = TensorBoardLogger(save_dir="./lightning_logs/", name=args.experiment_name, log_graph=True)
 callbacks = [
     ModelSummary(max_depth=2),
-    EarlyStopping(monitor="total_loss/train", min_delta=0.0, patience=15, mode="min"),
+    EarlyStopping(monitor="total_loss/train", min_delta=0.0, patience=40, mode="min"),
     ModelCheckpoint(monitor="accuracy/val/dataloader_idx_1", mode="max", verbose=True),
     tracker
 ]
