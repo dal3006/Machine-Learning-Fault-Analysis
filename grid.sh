6fcd07cd438c9ad9c7b82d77cc36cff5bd969540
Luca Rillosi <luca.rillosi+git@gmail.com>
2022-05-16 20:41:07 +0200

    add autoreload

---
+++ grid.sh
@@ -0,0 +1,2 @@
+grid run --use_spot --auto_resume --instance_type m5a.large --datastore_name cwru \
+trainer_main.py --accelerator auto
--- trainer_main.py
+++ trainer_main.py
@@ -5,6 +5,7 @@ from pytorch_lightning.callbacks import ModelSummary, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import Callback, ModelCheckpoint
 import pytorch_lightning as pl
+import os
 
 # PARSE ARGS
 parser = ArgumentParser()
@@ -19,34 +20,49 @@ parser = MyModel.add_argparse_args(parser)
 parser = pl.Trainer.add_argparse_args(parser)
 logparser = parser.add_argument_group("MyLogger")
 logparser.add_argument("--experiment_name", "-n", type=str, default="default")
+parser.add_argument('--autorestore', default="true", type=lambda x: (str(x).lower() in ['true', '1', 'yes']))
 args = parser.parse_args()
+
+# restore checkpoint
 
 # LOAD DATA
 data_module = MyDataModule.from_argparse_args(args)
 
 
-class CheckpointTracker(Callback):
 
-    def __init__(self):
-        self.collection = []
+# class CheckpointTracker(Callback):
 
-    def on_save_checkpoint(self, trainer, pl_module, checkpoint):
-        self.collection.append(trainer.callback_metrics)
+#     def __init__(self):
+#         self.collection = []
+
+#     def on_save_checkpoint(self, trainer, pl_module, checkpoint):
+#         self.collection.append(trainer.callback_metrics)
 
 
 # PREPARE TRAINER
-tracker = CheckpointTracker()
+# tracker = CheckpointTracker()
 logger = TensorBoardLogger(save_dir="./lightning_logs/", name=args.experiment_name, log_graph=True)
 callbacks = [
     ModelSummary(max_depth=2),
     EarlyStopping(monitor="accuracy/val/dataloader_idx_1", min_delta=0.0, patience=40, mode="max"),
-    ModelCheckpoint(monitor="accuracy/val/dataloader_idx_1", mode="max", verbose=True),
-    tracker
+    ModelCheckpoint(monitor="accuracy/val/dataloader_idx_1", mode="max", verbose=True), # save best target accuracy checkpoint
+    # tracker
 ]
-trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks, logger=logger, max_epochs=200)
+
+autorestore_file=None
+if args.autorestore:
+    callbacks.append(
+        ModelCheckpoint(dirpath="./autorestore/", filename=".autorestore",save_last=True), # Save last epoch for auto restore on grid.ai spot instances
+    )
+    if os.path.exists("./autorestore/last.ckpt"):
+        print("autorestore param true and restore file found.")
+        autorestore_file="./autorestore/last.ckpt"
+
+trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks, logger=logger, max_epochs=200, max_time={"hours": 2})
 
 # TRAIN
 my_model = MyModel(**vars(args))
-trainer.fit(my_model, data_module)
+trainer.fit(my_model, data_module, ckpt_path=autorestore_file)
 
-print("last_checkpoint_accu=" + str(float(tracker.collection[-1]['accuracy/val/dataloader_idx_1'])))
+
+# print("last_checkpoint_accu=" + str(float(tracker.collection[-1]['accuracy/val/dataloader_idx_1'])))
