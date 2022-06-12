4ff852fad0fcdb2af07f90ec4e184ba0204d7eed
Luca Rillosi <luca.rillosi@gmail.com>
2022-06-12 16:01:50 +0200

    add grid search

--- trainer_main.py
+++ trainer_main.py
@@ -3,13 +3,14 @@ from model import MyModel
 from data import MyDataModule
 from pytorch_lightning.callbacks import ModelSummary, EarlyStopping
 from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.callbacks import Callback, ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint
 import pytorch_lightning as pl
 import os
+import pandas as pd
+
 
 # PARSE ARGS
 # ---
-
 # Add args
 parser = ArgumentParser()
 parser = MyDataModule.add_argparse_args(parser)
@@ -18,8 +19,8 @@ parser = pl.Trainer.add_argparse_args(parser)
 logparser = parser.add_argument_group("MyLogger")
 logparser.add_argument("--experiment_name", "-n", type=str, default="default")
 parser.add_argument('--autorestore', default="true", type=lambda x: (str(x).lower() in ['true', '1', 'yes']))
+parser.add_argument('--grid_search', default="false", type=lambda x: (str(x).lower() in ['true', '1', 'yes']), help="Run grid search. Overrides --alpha and --beta.")
 parser.add_argument('--restore', type=str)
-
 # Read args
 args = parser.parse_args()
 
@@ -37,30 +38,55 @@ elif args.autorestore and os.path.exists("./autorestore/last.ckpt"):
 # ---
 data_module = MyDataModule.from_argparse_args(args)
 
-# CALLBACKS & LOGGERS
-# ---
-# Log to tensorboard
-logger = TensorBoardLogger(save_dir="./lightning_logs/", name=args.experiment_name, log_graph=True)
-# Save weights when model reaches best target accuracy
-bestaccu_callback = ModelCheckpoint(monitor="accuracy/val/dataloader_idx_1", mode="max", verbose=True)
-# Other callbacks
-callbacks = [
-    bestaccu_callback,
-    ModelSummary(max_depth=2),
-    # Early stop if accuracy stops improving
-    EarlyStopping(monitor="accuracy/val/dataloader_idx_1", min_delta=0.0, patience=60, mode="max"),
-    # Save most recent weights on each epoch
-    ModelCheckpoint(dirpath="./autorestore/", filename=".autorestore", save_last=True),
-]
 
-# START TRAINING
-# ---
+def train(args_dict):
 
-my_model = MyModel(**vars(args))
-trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks, logger=logger, max_time={"hours": 2})
-trainer.fit(my_model, data_module, ckpt_path=autorestore_file)
+    # CALLBACKS & LOGGERS
+    # ---
+    # Log to tensorboard
+    logger = TensorBoardLogger(save_dir="./lightning_logs/", name=args.experiment_name, log_graph=True)
+    # Save weights when model reaches best target accuracy
+    bestaccu_callback = ModelCheckpoint(monitor="accuracy/val/dataloader_idx_1", mode="max", verbose=True)
+    # Other callbacks
+    callbacks = [
+        bestaccu_callback,
+        ModelSummary(max_depth=2),
+        # Early stop if accuracy stops improving
+        EarlyStopping(monitor="accuracy/val/dataloader_idx_1", min_delta=0.0, patience=60, mode="max"),
+        # Save most recent weights on each epoch
+        ModelCheckpoint(dirpath="./autorestore/", filename=".autorestore", save_last=True),
+    ]
 
-# EVALUATE
+    # START TRAINING
+    # ---
+    my_model = MyModel(**args_dict)
+    trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks, logger=logger, max_time={"hours": 2})
+    trainer.fit(my_model, data_module, ckpt_path=autorestore_file)
+
+    # EVALUATE
+    # ---
+    best_accu = float(bestaccu_callback.best_model_score)
+    print(f'{best_accu=}')
+    return best_accu
+
+
+args_dict = vars(args)
+
+# GRID SEARCH
 # ---
-best_accu = float(bestaccu_callback.best_model_score)
-print(f'{best_accu=}')
+if args.grid_search:
+    print("grid search")
+    df = pd.DataFrame({'alpha': [],'beta': [], 'accuracy': []})
+    for a in [0, 1, 0.1, 0.01, 0.001, 0.0001]:
+        for b in [0, 1, 0.1, 0.01, 0.001, 0.0001]:
+            print(f"a={a}, b={b}")
+            # Override args
+            args_dict["alpha"] = a
+            args_dict["beta"] = b
+            # Train
+            best_accu = train(args_dict)
+            # Log results
+            df = df.append({'alpha': a, 'beta': b, 'accuracy': best_accu}, ignore_index=True)
+            df.to_csv('gridsearch.csv')
+else:
+    train(args_dict)
