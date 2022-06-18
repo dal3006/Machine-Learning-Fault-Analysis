fa92d297049fe9814dba12107fed28fd66b3bc67
Luca Rillosi <luca.rillosi@gmail.com>
2022-06-18 11:17:54 +0200

    reorganize dir structure

--- grid.sh
+++ grid.sh
@@ -1,5 +1,12 @@
-grid run --instance_type "g4dn.xlarge" --datastore_name "cwru" \
-trainer_main.py --data_dir "/datastores/" --accelerator gpu --gpus 1 --source CWRUA3 --target CWRUB3 --num_classes 3 --reuse_target true --autorestore false
+## GRID PARAMS
+# --use_spot --auto_resume
+# --instance_type "g4dn.xlarge"
+
+## TRAINER PARAMS
+# --accelerator gpu --gpus 1
+# --accelerator cpu
 
 
-#--use_spot --auto_resume
+grid run --datastore_name "cwru" \
+trainer_main.py --data_dir "/datastores/" --accelerator cpu --autorestore false  \
+    --grid_search true --save_embeddings false --experiment_name grid_search --learning_rate 1e-3 --max_epochs 80
