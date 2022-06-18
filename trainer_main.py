187c04d11482bb3bdca3e9e83ebce8d25a81035c
Luca Rillosi <luca.rillosi@gmail.com>
2022-06-18 12:25:47 +0200

    log lr + better parameter for autorestore

--- trainer_main.py
+++ trainer_main.py
@@ -1,7 +1,7 @@
 from argparse import ArgumentParser
 from model import MyModel
 from data import MyDataModule
-from pytorch_lightning.callbacks import ModelSummary, EarlyStopping
+from pytorch_lightning.callbacks import ModelSummary, EarlyStopping, LearningRateMonitor
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 import pytorch_lightning as pl
@@ -18,19 +18,20 @@ parser = MyModel.add_argparse_args(parser)
 parser = pl.Trainer.add_argparse_args(parser)
 logparser = parser.add_argument_group("MyLogger")
 logparser.add_argument("--experiment_name", "-n", type=str, default="default")
-parser.add_argument('--autorestore', default="true", type=lambda x: (str(x).lower() in ['true', '1', 'yes']))
 parser.add_argument('--grid_search', default="false", type=lambda x: (str(x).lower() in ['true', '1', 'yes']), help="Run grid search. Overrides --alpha and --beta.")
-parser.add_argument('--restore', type=str)
+parser.add_argument('--resume_chkp_path', type=str, help="Path to a specific checkpoint to restore. Must exist.")
+parser.add_argument('--resume_chkp_last', default="false", type=lambda x: (str(x).lower() in ['true', '1', 'yes']), help="Restore last checkpoint if exists. Useful for resuming training on spot instances.")
 # Read args
 args = parser.parse_args()
 
 # AUTORESTORE FOR GRID.AI SPOT INSTANCES
 # ---
 autorestore_file = None
-if args.restore:
-    autorestore_file = args.restore
-elif args.autorestore and os.path.exists("./autorestore/last.ckpt"):
-    print("autorestore param true and restore file found.")
+if args.resume_chkp_path:
+    print(f">>>>>>>>> Restoring from {args.resume_chkp_path} <<<<<<<<<")
+    autorestore_file = args.resume_chkp_path
+elif args.resume_chkp_last and os.path.exists("./autorestore/last.ckpt"):
+    print(">>>>>>>>>> Restoring last checkpoint <<<<<<<<<<")
     autorestore_file = "./autorestore/last.ckpt"
 
 
@@ -55,6 +56,7 @@ def train(args_dict):
         EarlyStopping(monitor="accuracy/val/dataloader_idx_1", min_delta=0.0, patience=60, mode="max"),
         # Save most recent weights on each epoch
         ModelCheckpoint(dirpath="./autorestore/", filename=".autorestore", save_last=True),
+        LearningRateMonitor()
     ]
 
     # START TRAINING
